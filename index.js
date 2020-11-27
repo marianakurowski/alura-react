efa15b7d8b00ddf50ac93cb2047112d8fdb91721
marianakurowski <mariana.rdsk@usp.br>
2020-11-26 22:34:58 -0300

    Create index.js
    Primeiros passos

---
+++ index.js
@@ -0,0 +1,41 @@
+class Cliente{
+    nome;
+    cpf;
+}
+
+class ContaCorrente{
+    agencia;
+    saldo = 0;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0 ){
+            this.saldo += valor;
+            console.log(this.#saldo);
+        }
+    }
+}
+
+const cliente1 = new Cliente()
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.sacar(50);
+
+
+console.log(contaCorrenteRicardo);
